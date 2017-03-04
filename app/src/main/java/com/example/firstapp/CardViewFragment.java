551d88c25766c1c7681330839d2e030b12c06bfc
reveriel <reveriel@hotmail.com>
2017-03-04 11:03:40 +0800

    three cardview with click effect

--- app/src/main/java/com/example/firstapp/CardViewFragment.java
+++ app/src/main/java/com/example/firstapp/CardViewFragment.java
@@ -64,46 +64,47 @@ public class CardViewFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_card_view, container, false);
     }
 
+
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mCardView = (CardView) view.findViewById(R.id.cardview);
-        mRadiusSeekBar = (SeekBar) view.findViewById(R.id.cardview_radius_seekbar);
-        mRadiusSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.d(TAG, String.format("SeekBar Radius progress : %d", progress));
-                mCardView.setRadius(progress);
-            }
+//        mRadiusSeekBar = (SeekBar) view.findViewById(R.id.cardview_radius_seekbar);
+//        mRadiusSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+//            @Override
+//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//                Log.d(TAG, String.format("SeekBar Radius progress : %d", progress));
+//                mCardView.setRadius(progress);
+//            }
+//
+//            @Override
+//            public void onStartTrackingTouch(SeekBar seekBar) {
+//                //Do nothing
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(SeekBar seekBar) {
+//                //Do nothing
+//            }
+//        });
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                //Do nothing
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                //Do nothing
-            }
-        });
-
-        mElevationSeekBar = (SeekBar) view.findViewById(R.id.cardview_elevation_seekbar);
-        mElevationSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.d(TAG, String.format("SeekBar Elevation progress : %d", progress));
-                mCardView.setElevation(progress);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                //Do nothing
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                //Do nothing
-            }
-        });
+//        mElevationSeekBar = (SeekBar) view.findViewById(R.id.cardview_elevation_seekbar);
+//        mElevationSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+//            @Override
+//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//                Log.d(TAG, String.format("SeekBar Elevation progress : %d", progress));
+//                mCardView.setElevation(progress);
+//            }
+//
+//            @Override
+//            public void onStartTrackingTouch(SeekBar seekBar) {
+//                //Do nothing
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(SeekBar seekBar) {
+//                //Do nothing
+//            }
+//        });
     }
 }
